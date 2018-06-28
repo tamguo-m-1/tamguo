c192f52bb38f5ea8c1b79662128f3ad242b6ec70
tamguo <tamguo@Lenovo>
2018-06-28 14:14:42 +0800

    短信验证码

--- tamguo/src/main/java/com/tamguo/service/impl/SmsService.java
+++ tamguo/src/main/java/com/tamguo/service/impl/SmsService.java
@@ -36,9 +36,9 @@ public class SmsService implements ISmsService{
         //必填:待发送手机号
         request.setPhoneNumbers(mobile);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("糖果购注册");
+        request.setSignName("探果网");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_10310548");
+        request.setTemplateCode("SMS_127152965");
         Integer vcode = (int) ((Math.random()*9+1)*100000);  
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         request.setTemplateParam("{\"code\":\""+vcode+"\"}");
